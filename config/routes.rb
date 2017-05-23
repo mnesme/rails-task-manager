3154cc942d20e7a0f225354f2342e4d5a5de2a5e
Maxence Nesme <maxencenesme@gmail.com>
2017-05-23 13:17:48 +0200

    routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+
+  get '/tasks_controller', to: 'tasks_controller#index'
+
+  get '/tasks_controller/:id', to: 'tasks_controller#show'
+
+  get '/tasks_controller/new', to:'tasks_controller#new'
+
+  post '/tasks_controller', to: 'tasks_controller#create'
+
+  get '/tasks_controller/:id/edit', to: 'tasks_controller#edit'
+
+  patch '/tasks_controller/:id', to: 'tasks_controller#update'
+
+  delete '/tasks_controller/:id', to: 'tasks_controller#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
